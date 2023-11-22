14516eaf03ce0568458464732d6d3261f872b17c
Navaneeth <navaneethkumar974@gmail.com>
2023-11-22 13:35:18 +0530

    newChanges

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,23 +1,15 @@
 import React from "react";
-// import bgslide from "../../src/vid/bgslide.mp4";
-// import { Grid} from '@mui/material';
-// import PostSlider from 'ui-component/Slider';
- 
-
 import image1 from "../assets/images/image1.jpg";
-
 
 export const Header = (props) => {
   return (
     <header id="header">
       <div className="intro">
-        {/* <video src={bgslide} autoPlay loop muted /> */}
-        {/* <Grid xs={12} sm={12} md={4.3} lg={4.3}>
-          <StyledBox>
-            <PostSlider medias={sliderData} />
-          </StyledBox>
-        </Grid> */}
-        <img src={image1} alt="image1.jpg" />
+        <img
+          src={image1}
+          alt="image1.jpg"
+          className="responsive-image" // Add this class for styling
+        />
         <div className="overlay">
           <div className="container">
             <div className="row">
@@ -29,7 +21,8 @@ export const Header = (props) => {
                 <p>{props.data ? props.data.paragraph : "Loading"}</p>
                 <a
                   href="#features"
-                  className="btn btn-custom btn-lg page-scroll">
+                  className="btn btn-custom btn-lg page-scroll"
+                >
                   Learn More
                 </a>{" "}
               </div>
